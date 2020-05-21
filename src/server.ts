fc040a58bd34c9db7dab812bd7e58b2e5e3436c9
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-05-21 11:08:27 +0200

    Add connection with neo4j

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,30 @@
+import 'graphql-import-node';
+
 import { ApolloServer } from 'apollo-server-express';
 import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
 import depthLimit from 'graphql-depth-limit';
 import { createServer } from 'http';
+import { driver } from 'neo4j-driver';
+import { makeAugmentedSchema } from 'neo4j-graphql-js';
 
-import schema from './schema';
+import * as typeDefs from './definitions/schema.graphql';
+
+// import typeDefs from './schema';
+
+const driverInstance = driver(
+  'bolt://localhost'
+);
 
 const app = express();
+
+const schema = makeAugmentedSchema({ typeDefs });
 
 const server = new ApolloServer({
   schema,
   validationRules: [depthLimit(7)],
+  context: { driver: driverInstance}
 });
 
 app.use('*', cors());
@@ -20,8 +33,9 @@ app.use(compression());
 server.applyMiddleware({ app, path: '/graphql' });
 
 const httpServer = createServer(app);
+const port = 4001;
 
 httpServer.listen(
-  { port: 4000 },
-  (): void => console.log(`\nGraphQL is now running on http://localhost:4000/graphql`)
+  { port },
+  (): void => console.log(`\nGraphQL is now running on http://localhost:${port}/graphql`)
 );
