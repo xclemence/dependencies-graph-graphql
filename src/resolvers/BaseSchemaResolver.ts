3ffd296ccc2cbeb481eeec57b29da496652f1e2c
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-06-29 00:20:13 +0200

    Remove unnecessary lines

--- src/resolvers/BaseSchemaResolver.ts
+++ src/resolvers/BaseSchemaResolver.ts
@@ -1,5 +1,5 @@
 import { GraphQLResolveInfo } from 'graphql';
-import { Neo4jContext, neo4jgraphql, RequestArguments } from 'neo4j-graphql-js';
+import { Neo4jContext, RequestArguments } from 'neo4j-graphql-js';
 
 import { executeCountQuery } from '../services/neo4j/count-query';
 
@@ -11,11 +11,6 @@ const resolvers = {
     AssemblyCount(object: any, params: RequestArguments, ctx: Neo4jContext, resolveInfo: GraphQLResolveInfo) {
       return executeCountQuery(params, ctx, resolveInfo, "Assembly");
     },
-    Assembly(object: any, params: RequestArguments, ctx: Neo4jContext, resolveInfo: GraphQLResolveInfo) {
-      console.log(JSON.stringify(resolveInfo));
-
-      return neo4jgraphql(object, params, ctx, resolveInfo, true);
-    }
   },
   Mutation: {
     hello: (parent: any, parameters: { name: String }, context: any, info: GraphQLResolveInfo): String => {
